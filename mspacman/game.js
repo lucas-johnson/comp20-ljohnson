e65dab12a96e6ed41401edf5c4eff0c15dbbe2e2
Lucas Johnson <lucas.johnson@tufts.edu>
2015-03-01 23:57:43 -0500

    started code for game.js

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,16 @@
-// Replace this with your code...
+<SCRIPT LANGUAGE = "JavaScript">
+
+functon init(){
+	canvas = document.getElementById('ms.pacman)';
+	if(canvas.getContext{
+		
+		img = new Image();
+		img.src = "pacman10-hp-sprite.png";
+		ctx = canvas.getContext('2d');
+		/* 785 x 275 */
+		ctx.drawImage(img)
+	}
+}
+
+
+</SCRIPT>
