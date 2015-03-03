9b13953ce9d1b20e44428bc31c9cf75746913a1e
Lucas Johnson <lucas.johnson@tufts.edu>
2015-03-02 20:43:30 -0500

    coded what I thought should work, but I am getting a blank canvas

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,16 +1,18 @@
-<SCRIPT LANGUAGE = "JavaScript">
-
-functon init(){
-	canvas = document.getElementById('ms.pacman)';
-	if(canvas.getContext{
+functon init() {
+	'use strict';
+	canvas = document.getElementById('game_canvas');
+	if(canvas.getContext) {
 		
 		img = new Image();
 		img.src = "pacman10-hp-sprite.png";
+		
 		ctx = canvas.getContext('2d');
-		/* 785 x 275 */
-		ctx.drawImage(img)
+	
+		ctx.drawImage(img,323, 2, 463, 136, 0, 0, 463, 136);
+
+		ctx.drawImage(img, 85, 25, 11, 11, 34, 34, 11, 11);
+	}
+	else{
+		alert('Canvas is not supported on this browser');
 	}
 }
-
-
-</SCRIPT>
