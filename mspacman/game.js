052fb015bc97b2a7ddcc3bf818978535aaa702a7
Lucas Johnson <lucas.johnson@tufts.edu>
2015-03-02 21:17:37 -0500

    this code produces the ms pacman sprite but not the game board

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,16 +1,24 @@
-functon init() {
-	'use strict';
-	canvas = document.getElementById('game_canvas');
+function init() {
+	var canvas = document.getElementById('game_canvas');
 	if(canvas.getContext) {
-		
-		img = new Image();
-		img.src = "pacman10-hp-sprite.png";
-		
-		ctx = canvas.getContext('2d');
-	
-		ctx.drawImage(img,323, 2, 463, 136, 0, 0, 463, 136);
 
-		ctx.drawImage(img, 85, 25, 11, 11, 34, 34, 11, 11);
+		var ctx = canvas.getContext('2d');
+		
+		var img = new Image();
+		//var board = new Image();
+		
+		//board.src = "pacman10-hp-sprite.png";
+		//mspacman.src = "pacman10-hp-sprite.png";
+		img.src = "pacman10-hp-sprite.png";
+
+		img.addEventListener("load", function() {
+	
+			ctx.drawImage(img, 323, 2, 464, 139, 0, 0, 464, 139);
+
+			ctx.drawImage(img, 83, 23, 13, 13, 34, 34, 13, 13);
+
+		}, false);
+
 	}
 	else{
 		alert('Canvas is not supported on this browser');
