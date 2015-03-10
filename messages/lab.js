bbbd96a5e650d5670959082c955a5bdefe82290f
Lucas Johnson <lucas.johnson@tufts.edu>
2015-03-10 01:31:05 -0400

    changed lab.js to request from data.json and added README.txt

--- messages/lab.js
+++ messages/lab.js
@@ -1,7 +1,7 @@
 function parse(){
 	request = new XMLHttpRequest(); 
 
-	request.open("GET", "http://messagehub.herokuapp.com/messages.json", true);
+	request.open("GET", "data.json", true);
 
 	request.onreadystatechange = parseData;
 
