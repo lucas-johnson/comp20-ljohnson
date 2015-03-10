4f59926acf2b69368dd13ecb88d68e0c20ed74d0
Lucas Johnson <lucas.johnson@tufts.edu>
2015-03-09 23:59:48 -0400

    fixing error: 'unexpected end of data at line 1 column 1 of the JSON data'

--- messages/lab.js
+++ messages/lab.js
@@ -15,7 +15,7 @@ function parseData(){
 		converted = JSON.parse(request.responseText);
 
 		for(i = 0; i < 2; i++){
-			document.getElementById("messages").innerHTML += "<p>" + converted["id"][i];
+			document.getElementById("messages").innerHTML += "<p>" + converted[i]["id"];
 		}
 	}else{
 		window.alert("Error!!!");
