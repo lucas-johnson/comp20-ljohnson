b845920252b18cf79757558773fa7293c72b3fcd
Lucas Johnson <lucas.johnson@tufts.edu>
2015-03-09 22:28:44 -0400

    Started java script code. Ready to test

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here...
+function parse(){
+	request = new XMLHttpRequest(); 
+
+	request.open("GET", "data.json", true);
+
+	request.onreadystatechange = parseData;
+
+	request.send();
+}
+
+function parseData(){
+	console.log("Got data back!");
+}
