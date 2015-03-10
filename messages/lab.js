d1059ccda3219d75e0556dfb9209aaee85eaa2cf
Lucas Johnson <lucas.johnson@tufts.edu>
2015-03-09 22:59:05 -0400

    trying to fix error in parseData()

--- messages/lab.js
+++ messages/lab.js
@@ -2,7 +2,7 @@ function parseData(){
 	converted = JSON.parse(request.responseText);
 
 	for(i = 0; i < 2; i++){
-		messagesDiv.innerHTML += "<p>" + converted[i]["id"];
+		index messages.innerHTML += "<p>" + converted[i]["id"];
 	}
 	
 }
